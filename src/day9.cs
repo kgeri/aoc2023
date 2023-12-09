37b671ad63488230ff150e91de4adb43e1091729
gergo <kiss.gergely@gmail.com>
2023-12-09 10:05:45 +0000

    day 9 part 2

--- src/day9.cs
+++ src/day9.cs
@@ -6,13 +6,21 @@ class Solution
 {
     static void Main(string[] args)
     {
-        var result1 = File.ReadAllLines("inputs/day9.txt")
+        var estimators = File.ReadAllLines("inputs/day9.txt")
             .Select(ValueHistory.Parse)
-            .Select(history => new Estimator(history))
-            .Select(estimator => estimator.Estimate())
-            .Sum();
+            .Select(history => new Estimator(history));
 
+        // Part 1
+        var result1 = estimators
+        .Select(estimator => estimator.EstimateNext())
+        .Sum();
         Console.WriteLine(result1);
+
+        // Part 2
+        var result2 = estimators
+        .Select(estimator => estimator.EstimatePrevious())
+        .Sum();
+        Console.WriteLine(result2);
     }
 }
 
@@ -23,6 +31,7 @@ record ValueHistory(long[] Values)
 
 class Estimator
 {
+    private readonly List<long> firstValues = [];
     private readonly List<long> lastValues = [];
 
     internal Estimator(ValueHistory history)
@@ -30,14 +39,25 @@ class Estimator
         long[] diffs = history.Values;
         while (diffs.Any(d => d != 0))
         {
+            firstValues.Add(diffs.First());
             lastValues.Add(diffs.Last());
             diffs = Diff(diffs);
         }
     }
 
-    public long Estimate() => lastValues.Sum();
+    public long EstimateNext() => lastValues.Sum();
+
+    public long EstimatePrevious()
+    {
+        long result = firstValues.Last();
+        for (int i = firstValues.Count - 2; i >= 0; i--)
+        {
+            result = firstValues[i] - result;
+        }
+        return result;
+    }
 
     private static long[] Diff(long[] values) => values.Zip(values.Skip(1), (a1, a2) => a2 - a1).ToArray();
 
-    public override string ToString() => $"Estimator: {string.Join(", ", lastValues)}";
+    public override string ToString() => $"Estimator: firstValues={string.Join(", ", firstValues)}, lastValues={string.Join(", ", lastValues)}";
 }
